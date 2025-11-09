7fde4b76f4486ea9632fcc79b83d0e0fb2428a6f
Athena-E <athena.eng2004@gmail.com>
2025-11-09 03:07:24 +0000

    implement checking rules for statements, functions and programs

--- src/frontend/typechecker/mod.rs
+++ src/frontend/typechecker/mod.rs
@@ -1,5 +1,6 @@
 // src/frontend/typechecker/mod.rs
 
+pub mod check;
 pub mod context;
 pub mod error;
 pub mod smt;
@@ -10,6 +11,7 @@ pub mod synthesize;
 mod tests;
 
 // Re-export commonly used types
+pub use check::{check_function, check_program, check_stmt, check_stmts};
 pub use context::{MutableBinding, TypingContext, VarBinding};
 pub use error::TypeError;
 pub use smt::check_provable;
