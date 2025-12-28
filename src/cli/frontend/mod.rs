8430bc1d41bd6969228b309e44333ca8e98c8298
Athena-E <athena.eng2004@gmail.com>
2025-12-28 16:39:24 +0000

    remove unused cli module

--- src/cli/frontend/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod cli;
-pub mod display;
-pub mod pipeline;
-
-pub use cli::{Config, read_source_file};
-pub use display::display_typed_program;
-pub use pipeline::{PipelineError, run_pipeline};
